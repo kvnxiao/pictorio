17e4e055e699936601bb84d8e88dec5bfcb0a596
Kevin Xiao <xlzqwerty1@gmail.com>
2020-12-18 21:58:45 -0500

    Add random word generator

---
+++ random/words.go
@@ -0,0 +1,30 @@
+package random
+
+import (
+	"bufio"
+	"math/rand"
+	"os"
+)
+
+var words []string
+
+func init() {
+	f, err := os.Open("words.txt")
+	if err != nil {
+		return
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+
+	_ = f.Close()
+}
+
+func Word() string {
+	return words[rand.Intn(len(words))]
+}
